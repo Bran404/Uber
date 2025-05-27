4e9c13355051dae1022b0fc49a85df56155a3b6d
FMB_Player <franbalella@hotmail.com.ar>
2025-05-27 08:31:03 -0300

    Added Direccion Realizados cambios en el formato original: - Añadido: - private String: altura - Añadido: - private String: ciudad - Añadido: - private String: pais - Añadido: - private String: codigoPostal

--- src/uber/Direccion.java
+++ src/uber/Direccion.java
@@ -1,5 +1,57 @@
 package uber;
 
 public class Direccion {
+    private String calle;
+    private String altura;
+    private String ciudad;
+    private String pais;
+    private String codigoPostal;
 
+    public Direccion(String calle, String altura, String ciudad, String pais, String codigoPostal) {
+        this.calle = calle;
+        this.altura = altura;
+        this.ciudad = ciudad;
+        this.pais = pais;
+        this.codigoPostal = codigoPostal;
+    }
+    
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
 }
