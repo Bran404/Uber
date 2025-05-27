1da5de445ca9cc8798d018c481c5e6fba53a4972
FMB_Player <franbalella@hotmail.com.ar>
2025-05-27 08:56:23 -0300

    Added Pasajero Notas: - Método de pago puede desarrollarse en caso de que sea tarjeta para incluir información propia de ella y un pasajero debería poder configurar múltiples métodos de pago, iniciando por suponer que pague en efectivo si no posee ninguno (Representado lo último en constructor alterno)

--- src/uber/Pasajero.java
+++ src/uber/Pasajero.java
@@ -1,5 +1,51 @@
 package uber;
 
 public class Pasajero {
+    private String nombre;
+    private ArrayList<Direccion> direcciones;
+    private MetodoDePago metodoDePago;
 
+    public Pasajero(String nombre, MetodoDePago metodoDePago) {
+        this.nombre = nombre;
+        this.direcciones = new ArrayList<Direccion>();
+        this.metodoDePago = metodoDePago;
+    }
+
+    public Pasajero(String nombre) {
+        this.nombre = nombre;
+        this.direcciones = new ArrayList<Direccion>();
+        this.metodoDePago = MetodoDePago.EFECTIVO;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Direccion> getDirecciones() {
+        return direcciones;
+    }
+
+    public void addDireccion(Direccion direccion) {
+        this.direcciones.add(direccion);
+    }
+
+    public void removeDireccion(Direccion direccion) {
+        this.direcciones.remove(direccion);
+    }
+
+    public Direccion getDireccion(int index) {
+        return this.direcciones.get(index);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public MetodoDePago getMetodoDePago() {
+        return metodoDePago;
+    }
+
+    public void setMetodoDePago(MetodoDePago metodoDePago) {
+        this.metodoDePago = metodoDePago;
+    }
 }
